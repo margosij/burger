be5dc54737a988da3c87226fa32819686a656e48
Joel Margosian <margosij@gmail.com>
2019-11-13 21:33:45 -0500

    uncommented several CRUD's in burger controller.js

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,59 +3,55 @@ var router = express.Router()
 var burgerSetup = require("../models/burger.js")
 
 router.get("/", function(req, res){
-    // console.log(burgerSetup)
     burgerSetup.selectAll(function(data){
-        // console.log(data, "data")
         var object = {
             burgers: data
         }
         // console.log(object)
-        res.render("index", {burgers: data} 
+        res.render("index", {burgers: data}
         // function (err, html){console.log(err, "burgercontroller  line 13")}
         )
     })
-    // res.send("Hello World")
 })
 
-// router.post("/", function(req, res){
-//     console.log("i need help with posting")
-//     burger.insertOne([
-//         "burger_name"
-//     ], [
-//         req.body.burger_name
-//     ], function(result){
-//         res.json({ id: result.insertID})
-//     }
+router.post("/", function(req, res){
+    burger.insertOne([
+        "burger_name"
+    ], [
+        req.body.burger_name
+    ], function(result){
+        res.json({ id: result.insertID})
+    }
 
-//     )
-// })
+    )
+})
 
-// router.put("/", function(req, res){
-//     var condition = "id = " + req.params.id
+router.put("/", function(req, res){
+    var condition = "id = " + req.params.id
 
-//     console.log("condition", condition)
+    console.log("condition", condition)
 
-//     burger.updateOne({
-//         devoured: req.body.devoured
-//     }, condition, function(result) {
-//         if (result.changedRows == 0){
-//             return res.status(404).end()
-//         } else{
-//             res.status(200).end()
-//         }
-//     })
-// })
+    burger.updateOne({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0){
+            return res.status(404).end()
+        } else{
+            res.status(200).end()
+        }
+    })
+})
 
-// router.delete("/", function(req, res) {
-//     var condition = "id = " + req.params.id;
+router.delete("/", function(req, res) {
+    var condition = "id = " + req.params.id;
   
-//     burger.delete(condition, function(result) {
-//       if (result.affectedRows == 0) {
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 module.exports = router
